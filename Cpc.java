e94bcf45c5e7b49815dccca47228799262cc2f88
Shakthishree11 <108cs23049@gmail.com>
2025-03-27 10:49:30 +0530

    shakthi

--- Cpc.java
+++ Cpc.java
@@ -2,6 +2,8 @@ public class Cpc
 {
     public static void main(String args[])
     {
-        System.out.println("hello welcome to se lab ")
+        System.out.println("hello welcome to se lab ");
+        System.out.println("new one created");
+
     }
 }
